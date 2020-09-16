d896049358fcf44e7ba2183d874484eb0dea6f3b
Lanzhou Jiang <lzjiang99@gmail.com>
2020-09-16 12:39:38 +1000

    add CardTests class

---
+++ BlackJack/Card.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace BlackJack
+{
+    public enum Rank
+    {
+      Ace,
+      Two,
+      Three,
+      Four,
+      Five,
+      Six,
+      Seven,
+      Eight,
+      Nine,
+      Ten,
+      Jack,
+      Queen,
+      King
+    }
+    
+    public enum Suit
+    {
+        Spade,
+        Heart,
+        Diamond,
+        Club
+    }
+    
+    
+    public class Card
+    {
+        public Card(Rank rank, Suit suit)
+        {
+            Rank = rank;
+            Suit = suit;
+        }
+
+        public Rank Rank { get; private set; }
+        public Suit Suit { get; private set; }
+
+        public String DisplayCard()
+        {
+            String card = Rank.ToString() + Suit.ToString();
+            return card;
+        }
+
+    }
+}
--- BlackJackTests/CardTests.cs
+++ BlackJackTests/CardTests.cs
@@ -3,7 +3,7 @@ using Xunit;
 
 namespace BlackJackTests
 {
-    public class UnitTest1
+    public class CardTests
     {
         [Fact]
         public void Test1()
