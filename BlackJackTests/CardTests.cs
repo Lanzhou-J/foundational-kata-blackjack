7b949786842429da5b008088cefa78e087dc3d04
Lanzhou Jiang <lzjiang99@gmail.com>
2020-09-21 17:27:37 +1000

    add more test cases in CardTests class

--- BlackJackTests/CardTests.cs
+++ BlackJackTests/CardTests.cs
@@ -16,12 +16,15 @@ namespace BlackJackTests
             Assert.True(isMatch);
         }
         
-        [Fact]
-        public void FormatCardStringShould_ReturnCorrectString()
+        [Theory]
+        [InlineData(CardFace.Ace, Suit.Diamond, "[Ace, Diamond]")]
+        [InlineData(CardFace.Six, Suit.Heart, "[Six, Heart]")]
+        [InlineData(CardFace.King, Suit.Spade, "[King, Spade]")]
+        public void FormatCardStringShould_ReturnCorrectString(CardFace cardFace, Suit suit, string expectedResult)
         {
-            Card newCard = new Card(CardFace.Ace, Suit.Diamond);
+            Card newCard = new Card(cardFace, suit);
             
-            Assert.Equal("[Ace, Diamond]", newCard.FormatCardString());
+            Assert.Equal(expectedResult, newCard.FormatCardString());
         }
     }
 }
