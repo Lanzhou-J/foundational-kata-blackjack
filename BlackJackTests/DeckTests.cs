29801c56c232b423a40760ac3f9b373dae4384c4
Lanzhou Jiang <lzjiang99@gmail.com>
2020-09-23 14:33:36 +1000

    Add test case for CreateADeck() function in Deck class, test is passed, the function return a List of Card with correct number of items

--- BlackJackTests/DeckTests.cs
+++ BlackJackTests/DeckTests.cs
@@ -1,7 +1,17 @@
+using System.Collections.Generic;
+using BlackJack;
+using Xunit;
+
 namespace BlackJackTests
 {
     public class DeckTests
     {
-        
+        [Fact]
+        public void CreateADeckShould_ReturnAListOfCardsWithCorrectNumberOfItems()
+        {
+            Deck newDeck = new Deck();
+            List<Card> listOfCards = newDeck.CreateADeck();
+            Assert.Equal(52, listOfCards.Count); 
+        }
     }
 }
